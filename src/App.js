db87367b48127f69563ec4866c0b6210341afb1c
kmfoysal <faisal.cri@gmail.com>
2021-11-02 14:08:58 +0600

    Intigrated MUI & adding google fonts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Home from "./pages/Home/Home";
+import Navigation from "./pages/Home/Shared/Navigation/Navigation";
 
 function App() {
   return (
     <div className="">
       <Router>
+        <Navigation></Navigation>
         <Switch>
           <Route exact path='/'>
             <Home></Home>
